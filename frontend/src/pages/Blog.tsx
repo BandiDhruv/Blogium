a521e00f0edde05644cfda01560360485c9e354b
Dhruv Bandi <dhruvbandi03@gmail.com>
2024-04-27 00:13:31 +0530

    some type error in blog.tsx fixed

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -6,7 +6,7 @@ import { useParams } from "react-router-dom";
 export const Blog = () => {
   const {id}=useParams()
   const {blog ,loading}=useBlog({id:id || ""});
-  if(loading){
+  if(loading || !blog){
     return <div className="h-screen w-full flex flex-col justify-center items-center"><Spinner /></div>
   }
 
